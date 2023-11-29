3ae53fa22289edb52674f51e3c527ca45493c1d6
vedha <vedh1305@gmail.com>
2023-11-29 16:00:23 +0530

    Update

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 import App from './App';
-import imageData from "/components/DataComponent";
-// import AppClass from "./AppClass";
+import AppClass from "./AppClass";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App imageData= {imageData}/>   
-    {/* <AppClass imageData= {imageData}/>   */}
+    <App />
+    {/* <AppClass/> */}
     {/* change the above line - for interchaning between App and AppClass */}
   </React.StrictMode>
 );
