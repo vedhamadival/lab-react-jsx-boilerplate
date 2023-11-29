1a0077e333ca1b4c47252bf57559039b6560e7db
vedha <vedh1305@gmail.com>
2023-11-29 15:56:55 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,47 @@
-import './App.css';
+import "./App.css";
 import elephant from "./images/elephant.jpeg";
 
+const imageData = () => {
+  let data = [
+    {
+      id: 1,
+      img: elephant,
+    },
+    {
+      id: 2,
+      img: elephant,
+    },
+    {
+      id: 3,
+      img: elephant,
+    },
+    {
+      id: 4,
+      img: elephant,
+    },
+  ];
+  return data;
+};
 
-function App(props) {
+function App() {
   // code here
-  const {imageData}= props;
+
   const data = imageData();
 
-  return(
+  return (
     <div>
       <h1>Kalvium Gallery</h1>
       <div id="grid">
-        {data.map((el)=>{
-          return(
+        {data.map((el) => {
+          return (
             <div key={el.id}>
-              <img src={el.img}/>
-              </div>
+              <img src={el.img} />
+            </div>
           );
         })}
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -1,25 +1,47 @@
 import { Component } from "react";
-import "./App.css"
+import "./App.css";
 import elephant from "./images/elephant.jpeg";
 
-export default class AppClass extends Component{
-  
+export default class AppClass extends Component {
+  imageData = () => {
+    let data = [
+      {
+        id: 1,
+        img: elephant,
+      },
+      {
+        id: 2,
+        img: elephant,
+      },
+      {
+        id: 3,
+        img: elephant,
+      },
+      {
+        id: 4,
+        img: elephant,
+      },
+    ];
+    return data;
+  };
+
   // code here
-  render(){
-    const data = this.props.imageData();
-        return(
-          <div>
-            <h1>Kalvium Gallery</h1>
-            <div id="grid">
-              {data.map((el)=>{
-                return(
-                  <div key={el.id}>
-                    <img src={el.img}/>
-                    </div>
-                );
-              })}
-            </div>
-          </div>
-        )
-      }
+  render() {
+    const data = this.imageData();
+
+    return (
+      <div>
+        <h1>Kalvium Gallery</h1>
+        <div id="grid">
+          {data.map((el) => {
+            return (
+              <div key={el.id}>
+                <img src={el.img} />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
